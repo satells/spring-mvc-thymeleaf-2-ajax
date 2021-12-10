b24fcfef8827f4d7cc2b2630ddbea02639bee7ee
satells <msergiost@hotmail.com>
2021-12-10 15:12:31 -0300

    .

--- ajax/src/main/java/com/ajax/web/controller/PromocaoController.java
+++ ajax/src/main/java/com/ajax/web/controller/PromocaoController.java
@@ -10,14 +10,18 @@ import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ajax.repository.CategoriaRepository;
 import com.ajax.repository.PromocaoRepository;
@@ -51,7 +55,6 @@ public class PromocaoController {
 
 	@PostMapping("/save")
 	public ResponseEntity<?> salvar(@Valid Promocao promocao, BindingResult result) {
-//		LOG.info("Promocao {}", promocao.toString());
 
 		if (result.hasErrors()) {
 			Map<String, String> erros = new HashMap<String, String>();
@@ -67,9 +70,32 @@ public class PromocaoController {
 
 		promocao.setDtCadastro(LocalDateTime.now());
 
+//		LOG.info("Promocao {}", promocao.toString());
 		promocaoRepository.save(promocao);
 
 		return ResponseEntity.ok().build();
+	}
+
+	@GetMapping("/list")
+	public String listarOfertas(ModelMap model) {
+
+		Sort sort = Sort.by(Sort.Direction.DESC, "dtCadastro");
+		PageRequest pageRequest = PageRequest.of(0, 8, sort);
+
+		model.addAttribute("promocoes", promocaoRepository.findAll(pageRequest));
+
+		return "promo-list";
+	}
+
+	@GetMapping("/list/ajax")
+	public String listarCards(@RequestParam(name = "page", defaultValue = "1") int page, ModelMap model) {
+
+		Sort sort = Sort.by(Sort.Direction.DESC, "dtCadastro");
+		PageRequest pageRequest = PageRequest.of(page, 8, sort);
+
+		model.addAttribute("promocoes", promocaoRepository.findAll(pageRequest));
+
+		return "promo-card";
 	}
 
 }
---
+++ ajax/src/main/resources/static/js/promo-list.js
@@ -0,0 +1,112 @@
+// infinit scroll
+var pageNumber = 0;
+
+$(document).ready(function(){
+	$("#loader").hide();
+	$("#fim-btn").hide();
+});
+
+
+
+
+
+
+$(window).scroll(function(){
+	
+	var scrollTop = $(this).scrollTop();
+	var conteudo = $(document).height() - $(window).height();
+	//console.log(`scrolltop: ${scrollTop} | conteudo: ${conteudo}`);
+	
+	if (scrollTop >= conteudo) {
+		pageNumber++;
+		
+		//delay do javascript atraso de 200 milisegundos
+		setTimeout(function(){
+			loadByScrollBar(pageNumber);
+		}, 200);
+	}
+	
+});
+
+
+
+function loadByScrollBar(pageNumber){
+	
+	$.ajax({
+		method:"GET",
+		url:"/promocao/list/ajax",
+		data:{
+			page: pageNumber,
+		},
+		befoSend:function(){
+			$("#loader-img").show();
+		},
+		
+		
+		success:function(response){
+			var tamanhoDoObjetoDeResposta = 150;
+			//tamanho do objeto de resposta 
+			if(response.length>tamanhoDoObjetoDeResposta){
+//				console.log(`resposta > ${response}   ${response.length}`);
+				console.log(`resposta > ${response.length}`);
+				//carrega de form gradual
+				$(".row").fadeIn(250, function(){
+					$(this).append(response);
+ 				});
+			}else{
+				$("#fim-btn").show();
+				$("#loader-img").removeClass("loader");
+			}
+		},
+		complete:function(){
+			$("#loader-img").hide();
+		},
+		error:function(xhr){
+			console.log(`Ops, ocorreu um erro: ${xhr.status} - ${xhr.statusText}`);
+		},
+	});
+}
+//Adicionar likes
+
+//O * indica ao jQuery esperar um clique em qualquer botão que tenha a instrução "likes-btn-" 
+$(document).on("click", "button[id*='likes-btn-']",function(){
+	var id = $(this).attr("id").split("-")[2];
+	console.log(`id: ${id}`);
+	
+	
+	// ver eventos delegados - delegar ao jQuery os objetos adcionados posteriormente à criação do objeto DOM  
+	$.ajax({
+		method:"POST",
+		url:`/promocao/liks/${id}`,
+		success:function(response){
+			$(`#likes-count-${id}`).text(response);
+		},
+		error:function(xhr){
+			console.log(`Ocorreu, um erro: ${xhr.status} - ${xhr.statusText}`);
+		}
+	});
+});
+
+
+
+
+//$("button[id*='likes-btn-']").on("click",function(){
+//	var id = $(this).attr("id").split("-")[2];
+//	console.log(`id: ${id}`);
+//	
+//	
+//	// ver eventos delegados - delegar ao jQuery os objetos adcionados posteriormente à criação do objeto DOM  
+//	$.ajax({
+//		method:"POST",
+//		url:`/promocao/liks/${id}`,
+//		success:function(response){
+//			$(`#likes-count-${id}`).text(response);
+//		},
+//		error:function(xhr){
+//			console.log(`Ocorreu, um erro: ${xhr.status} - ${xhr.statusText}`);
+//		}
+//	});
+//});
+
+
+
